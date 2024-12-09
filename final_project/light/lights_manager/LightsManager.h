aab9d6b88cc0d4971a39e6cd9b6f87eeff721028
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-12-09 18:20:39 +0000

    feat: Creation of a light Manager to compute the depth

---
+++ final_project/light/lights_manager/LightsManager.h
@@ -0,0 +1,41 @@
+//
+// Created by miche on 07/12/2024.
+//
+
+#ifndef LIGHTS_MANAGER_H
+#define LIGHTS_MANAGER_H
+#include <vector>
+#include <light/light/Light.h>
+
+#include "glad/gl.h"
+#include "objects/graphics_object/GraphicsObject.h"
+
+
+class LightsManager {
+    private:
+        // Depth perspective settings
+        float depthFoV = 90.0f;
+        float depthNear = 200.0f;
+        float depthFar = 800.0f;
+
+        GLuint depthTexturesArray = -1;
+        GLuint depthFBO = -1;
+        GLuint lightsUBO;
+        std::vector<Light> lights;
+        std::vector<GraphicsObject *> objects;
+
+        void loadDepthTextures();
+        void loadLightsUBOs();
+
+        void createDepthTextureArray();
+
+    public:
+        explicit LightsManager(const std::vector<Light> &lights, const std::vector<GraphicsObject *>& objects);
+
+        void render();
+        void clean() const;
+};
+
+
+
+#endif //LIGHTS_MANAGER_H
